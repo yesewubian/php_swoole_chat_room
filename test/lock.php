f4a83ae8802d8d262c05e80cd84d74042ad41f47
lishaoqiang <lishaoqiang@zhaomoapp.com>
2020-05-12 10:55:22 +0800

    add lock test

--- test/lock.php
+++ test/lock.php
@@ -9,16 +9,20 @@ if(pcntl_fork()>0){
     sleep(3);
     $lock->unlock();
 }else{
+
+    //阻塞
 //    echo "[child] wait lock\n";
 //    $lock->lock();
 //    echo "[child] get lock\n";
 //    $lock->unlock();
 //    exit("[child] exit\n");
+
+    //非阻塞
     if($lock->trylock()){
-        echo '[child] get lock successful';
+        echo "[child] get lock successful\n";
         $lock->unlock();
     }else{
-        echo '[child] get lock fail';
+        echo "[child] get lock fail\n";
     }
     exit("[child] exit\n");
 }
